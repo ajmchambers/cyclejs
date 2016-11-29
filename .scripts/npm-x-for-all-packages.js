56e521ecb437cc1425758cb6db316d382d29f13c
Alan Chambers <ajmchambers@gmail.com>
2016-11-29 21:08:29 +0000

    tidied up npm-x script to match style of other scripts

--- .scripts/npm-x-for-all-packages.js
+++ .scripts/npm-x-for-all-packages.js
@@ -1,29 +1,41 @@
 #!/usr/bin/env node
 
-'use strict'
+/** This script runs an npm command for every registered package
+ * in the file `RELEASABLE_PACKAGES`.
+ *
+ * E.g.: calling this script will the command line args "run test"
+ * will execute `npm run test` for each package.
+*/
 
-var fs = require('fs')
-var path = require('path')
-var execSync = require('child_process').execSync
-var argArray = process.argv.slice(2)
-var command = argArray.join(' ')
-var dirArray = [];
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var execSync = require('child_process').execSync;
+
+var argArray = process.argv.slice(2);
+var command = argArray.join(' ');
+
+var releasablePackages = fs
+  .readFileSync(path.resolve(__dirname, 'RELEASABLE_PACKAGES'), 'utf8')
+  .trim()
+  .split(/\r?\n/);
 
 function handleError(error) {
   switch(error.code) {
     case 'ENOENT':
-      console.error('No such file or directory: ' + error.options.cwd )
-      break
+      console.error('No such file or directory: ' + error.options.cwd );
+      break;
     default:
-      console.error('Error: \n' + err.message)
-      break
+      console.error('Error: \n' + err.message);
+      break;
   }
 }
 
 function runCommand(command) {
   return function runCommandIn(dir) {
-    console.log('> (' + dir + ')')
-    console.log("> npm " + command + '\n')
+    console.log('> (' + dir + ')');
+    console.log("> npm " + command + '\n');
     try {
       execSync(
         'npm ' + command, 
@@ -31,26 +43,21 @@ function runCommand(command) {
           cwd: path.resolve(dir),
           stdio: 'inherit'
         }
-      )
+      );
     } catch(err) {
-      handleError(err)
+      handleError(err);
     }
   }
 }
 
 if (argArray.length === 0)  {
-  console.error('No command supplied')
-  process.exit()
+  console.error('No command supplied');
+  process.exit();
 }
 
-dirArray = fs
-  .readFileSync(path.resolve(__dirname, 'RELEASABLE_PACKAGES'), 'utf8')
-  .split(/\r?\n/)
-  .filter(function(dir) { return dir.length !== 0 })
-
-if (dirArray.length === 0)  {
-  console.error('No directories supplied')
-  process.exit()
+if (releasablePackages.length === 0)  {
+  console.error('No directories supplied');
+  process.exit();
 }
 
-dirArray.forEach(runCommand(command))
+releasablePackages.forEach(runCommand(command));
